ea8525b7cfac67afa60e8468efdd6292ffc5ae76
MSLacerda <matheus_lacerda_2@carrefour.com>
2025-10-31 19:51:07 -0300

    [change] adicioandno coisinhas

--- python/blackjack/main.py
+++ python/blackjack/main.py
@@ -1,39 +1,48 @@
 ### Creating a Class Card
+import logging
+from typing import Literal
+
 class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.rank_val_map = {
-            'A':11, 
-            '2':2, 
-            '3':3, 
-            '4':4,
-            '5':5,
-            '6':6,
-            '7':7,
-            '8':8,
-            '9':9,
-            '10':10,
-            'K':10,
-            'Q':10,
-            'J':10
-        }
-        self.suit = suit
-        if suit not in ['♠','♥','♦','♣']:
-            raise Exception('Invalid suit')
+    RANKS: dict[str, int] = {
+        'A':11, 
+        '2':2, 
+        '3':3, 
+        '4':4,
+        '5':5,
+        '6':6,
+        '7':7,
+        '8':8,
+        '9':9,
+        '10':10,
+        'K':10,
+        'Q':10,
+        'J':10
+    }
+    SUITS: list[str] = ['♠','♥','♦','♣']
+
+    def __init__(
+            self, 
+            rank: Literal['A','2','3','4','5','6','7','8','9','10','J','Q','K'], 
+            suit: Literal['♠','♥','♦','♣']
+        ):
+        self.rank: str = rank
+        self.suit: str = suit
+        if suit not in self.SUITS:
+            raise ValueError(f'Invalid suit!\nChoose one of {",".join(self.SUITS)}')
 
     @property
     def value(self):
         try: 
-            value: int = self.rank_val_map[self.rank]
+            value: int = self.RANKS[self.rank]
         except KeyError:
-            raise KeyError('Invalid rank')
+            raise ValueError(f'Invalid rank!\nChoose one of {",".join([key for key in self.RANKS.keys()])}')
         return value
     
     def __repr__(self):
         card: str = self.rank + self.suit
         return card
 
-
-a = Card('J', '♠')
-print(a.value)
-print(a)
+if __name__ == '__main__':
+    a: Card = Card('J', '♠')
+    logging.info(a.value)
+    logging.info(a)
