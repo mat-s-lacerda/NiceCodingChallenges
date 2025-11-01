deebd5ec6f79d25dbe9179507eb0a95f0ff8b84d
AnaF <anaflavia.araujo1997@gmail.com>
2025-11-01 19:56:22 -0300

    finalizacao classe deck

--- python/blackjack/deck.py
+++ python/blackjack/deck.py
@@ -1,17 +1,37 @@
 from card import Card
+from random import shuffle
 class Deck:
     def __init__(
         self
     ):
-        self.cards: list[Card] = [] 
+        self.set_deck()
+
+    def create(self):
+        cards: list[Card] = [] 
         for suit in Card.SUITS:
             for rank in Card.RANKS.keys():
                 card: Card = Card(rank=rank,suit=suit)
-                self.cards.append(card)
-            
+                cards.append(card)
+        return cards
 
+    def shuffle(self):
+        shuffle(self.cards)
 
-a = Deck()
-print(a.cards)
+    def set_deck(self):
+        self.cards = self.create()
+        self.shuffle()
+
+    def draw(self):
+        if len(self.cards) <= 10:
+            self.set_deck()
+        card: Card = self.cards.pop()
+        return card
+
+    
+
+#a = Deck()
+#while True:
+#    print(len(a.cards))
+#    a.draw()
 
 
